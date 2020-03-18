4ae34a54f95459a04f7bdb22a1b62ded04566b1a
Chris <chris.gonzales.82@gmail.com>
2020-03-18 10:40:17 -0700

    Added GameAIUnitTest Case Class

---
+++ TicTacToeTests/GameAITests.swift
@@ -0,0 +1,18 @@
+//
+//  GameAITests.swift
+//  TicTacToeTests
+//
+//  Created by Chris Gonzales on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameAITests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
