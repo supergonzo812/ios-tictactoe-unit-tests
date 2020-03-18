77d043048830b483cd5d9599b55f0a96e16fe37e
Chris <chris.gonzales.82@gmail.com>
2020-03-18 12:20:25 -0700

    Guided project done

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -13,19 +13,39 @@ func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {
     // checking vertical column 0
     var numberOfMarks = 0
     
+    for x in 0...2 {
+        for y in 0...2 {
+            if board[(x, y)] == player {
+                numberOfMarks += 1
+            }
+        }
+        if numberOfMarks == 3 {
+        return true
+        }
+    }
+    
+    // checking horizontal rows
     for y in 0...2 {
-        if board[(0, y)] == player {
+        for x in 0...2 {
+            if board[(x, y)] == player {
+                numberOfMarks += 1
+            }
+        }
+        if numberOfMarks == 3 {
+        return true
+        }
+    }
+    
+    // checking horizontals
+    let leftToRight: [Coordinate] = [(0, 0), (1, 1), (2, 2)]
+    var numberOfMarks = 0
+    
+    for coordinate in leftToRight {
+        if board[Coordinate] == player {
             numberOfMarks += 1
         }
     }
     
-    if numberOfMarks == 3 {
-    
-    return true
-    }
-    
-    // check another column
-    numberOfMarks = 0
     
     
     return false
